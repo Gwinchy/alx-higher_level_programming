714fb8319569975104e4e78129f0d5d42a7b8efe
Gwinchy <gmunyui254@gmail.com>
2024-01-03 13:17:00 -0800

    one loop

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-alphabet = ''.join('{}' for _ in range(ord('a'), ord('z') + 1)).format(*(chr(i) for i in range(ord('a'), ord('z') + 1)))
+alphabet = ''.join('{}'.format(chr(i)) for i in range(ord('a'), ord('z') + 1))
 print(alphabet, end='')
